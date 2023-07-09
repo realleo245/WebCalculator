8ff511edc401f13ba5360a5601edcaae7e43ea31
realleo245 <s-llian@lwsd.org>
2023-07-08 19:02:09 -0700

    Created dark mode that kind of works

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var darkMode = window.matchMedia('prefers-color-scheme: dark');
+function toggleDarkMode(startup) {
+    var buttons = document.getElementsByClassName("btn");
+    for(var i = 0; i < buttons.length; i++) {
+        if(darkMode && startup) {
+            console.log("Enabling dark mode...");
+            buttons[i].classList.add("btn-dark");
+        }
+        else if(!darkMode && startup) {
+            console.log("Enabling light mode...");
+            buttons[i].classList.add("btn-light");
+        }
+        else if(darkMode && !startup) {
+            console.log("Enabling light mode...");
+            buttons[i].classList.add("btn-light");
+        }
+        else {
+            console.log("Enabling dark mode...");
+            buttons[i].classList.add("btn-dark");
+        }
+    }
+    
+}
