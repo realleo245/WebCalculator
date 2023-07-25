0c26a3ba11e40f35d8728ed603a7e80bd3d688d9
realleo245 <114704320+realleo245@users.noreply.github.com>
2023-07-24 20:44:42 -0700

    History works now!

--- js/history.js
+++ js/history.js
@@ -1,6 +1,12 @@
+var numEntries = 0;
 function updateHistory() {
     var table = document.getElementById("history");
     var entry = table.insertRow(-1);
     var key = entry.insertCell(-1);
     var value = entry.insertCell(-1);
+    var input = window.localStorage.getItem(numEntries);
+    key.innerHTML = input.split(":")[0];
+    value.innerHTML = input.split(":")[1];
+    numEntries++;
+    console.log("History updated with" + key.innerHTML + value.innerHTML); 
 }
